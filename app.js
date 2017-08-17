fa1f59077bdaa5279ccc9795ce19349a8f2d4c6d
adriennekarnoski <adrienne.j.karnoski@gmail.com>
2017-08-16 20:48:02 -0700

    added tab based navigation

--- app.js
+++ app.js
@@ -1,1 +1,21 @@
 'use strict';
+
+function handleNav() {
+  $('nav').on('click', 'li', function() {
+    var $navValue = $(this).data('content');
+    console.log($navValue);
+    if ($navValue === 'projects') {
+      $('.section-container').hide();
+      $('section#projects').show();
+    } else if ($navValue === 'about') {
+      $('.section-container').hide();
+      $('section#about-me').show();
+      $('section#skills').show();
+    } else {
+      $('.section-container').show();
+    }
+
+  });
+}
+
+handleNav();
