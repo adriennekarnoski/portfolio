78e1038710d5f42bdcc6bd44c13235b94631544c
adriennekarnoski <adrienne.j.karnoski@gmail.com>
2017-08-17 17:46:34 -0700

    added smacss and updating navigation

--- app.js
+++ app.js
@@ -1,19 +1,30 @@
 'use strict';
 
+// function handleNav() {
+//   $('nav').on('click', 'li', function() {
+//     var $navValue = $(this).data('content');
+//     console.log($navValue);
+//     if ($navValue === 'projects') {
+//       $('.section-container').hide();
+//       $('section#projects').show();
+//     } else if ($navValue === 'about') {
+//       $('.section-container').hide();
+//       $('section#about-me').show();
+//       $('section#skills').show();
+//     } else {
+//       $('.section-container').show();
+//     }
+//
+//   });
+// }
+//
+// handleNav();
+
 function handleNav() {
-  $('nav').on('click', 'li', function() {
-    var $navValue = $(this).data('content');
-    console.log($navValue);
-    if ($navValue === 'projects') {
-      $('.section-container').hide();
-      $('section#projects').show();
-    } else if ($navValue === 'about') {
-      $('.section-container').hide();
-      $('section#about-me').show();
-      $('section#skills').show();
-    } else {
-      $('.section-container').show();
-    }
+  $('#nav-list').on('click', 'i.fa-bars', function() {
+    var $navList = $('ul');
+    console.log($navList);
+    $navList.show();
 
   });
 }
