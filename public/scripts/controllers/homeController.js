cf5855dfb7340e4d2aa6611722af155bdfd5ff9f
adriennekarnoski <adrienne.j.karnoski@gmail.com>
2017-08-28 20:21:19 -0700

    set up portfolio with single page application layout and pages.js

---
+++ public/scripts/controllers/homeController.js
@@ -0,0 +1,14 @@
+'use strict';
+var app = app || {};
+
+(function(module) {
+  const homeController = {};
+
+  homeController.init = function() {
+    var $navList = $('.nav-ul');
+    $navList.hide();
+  $('.section-container').fadeIn();
+  };
+
+  module.homeController = homeController;
+})(app);
--- server.js
+++ server.js
@@ -5,6 +5,10 @@ const app = express();
 const PORT = process.env.PORT || 3333;
 app.use(express.static('./public'));
 
+app.get('*', (req, res) => {
+  res.sendFile('index.html', { root: './public' })
+})
+
 app.listen(PORT, function() {
   console.log('Listening on port ' + PORT);
 });
