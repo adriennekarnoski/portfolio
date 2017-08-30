5e4ebabed726f5a0ebb1e762636b9c8eace79e41
adriennekarnoski <adrienne.j.karnoski@gmail.com>
2017-08-29 21:25:12 -0700

    working on github api

--- public/scripts/controllers/projectsController.js
+++ public/scripts/controllers/projectsController.js
@@ -9,7 +9,10 @@ var app = app || {};
     $navList.hide();
     $('.section-container').hide();
     $('section#projects').fadeIn('slow');
+    $('section#project-repo').fadeIn('slow');
   };
+
+app.repos.requestRepos(app.repoView.index);
 
   module.projectsController = projectsController;
 })(app);
