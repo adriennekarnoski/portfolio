7d5e55a37a5d882ad877f38aa48a4dadbd65c411
adriennekarnoski <adrienne.j.karnoski@gmail.com>
2017-08-30 17:29:07 -0700

    added dependancy and fixed bug in proxy

--- public/scripts/models/repo.js
+++ public/scripts/models/repo.js
@@ -7,16 +7,15 @@ var app = app || {};
   repos.all = [];
 
   repos.requestRepos = function(callback) {
-  $.get('/github/user/repos')
-          .then(
-            data => data.forEach(function(ele) {repos.all.push(ele)}),
-
-            err => console.error(err))
-            .then(callback)
+$.get('/github/user/repos')
+.then(data => data.forEach(function(ele) {repos.all.push(ele)}),
+err => console.error(err))
+.then(callback);
 };
-
 
 repos.with = attr => repos.all.filter(repo => repo[attr]);
 
   module.repos = repos;
 })(app);
+
+// data.forEach(function(ele) {repos.all.push(ele)}
--- server.js
+++ server.js
@@ -3,21 +3,21 @@
 const express = require('express');
 const app = express();
 const PORT = process.env.PORT || 3333;
+const requestProxy = require('express-request-proxy');
 app.use(express.static('./public'));
+
+app.get('/github/*', proxyGitHub);
 
 app.get('*', (req, res) => {
   res.sendFile('index.html', { root: './public' })
 })
 
 function proxyGitHub(request, response) {
-  console.log('Routing GitHub request for', request.params[0]);
   (requestProxy({
     url: `https://api.github.com/${request.params[0]}`,
     headers: {Authorization: `token ${process.env.GITHUB_TOKEN}`}
   }))(request, response);
 }
-
-app.get('/github/*', proxyGitHub);
 
 app.listen(PORT, function() {
   console.log('Listening on port ' + PORT);
